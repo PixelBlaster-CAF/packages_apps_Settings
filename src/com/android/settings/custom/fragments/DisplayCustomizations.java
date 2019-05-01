3aae307f140e8ef59a3a5afec48ff44c28fe2638
Ido Ben-Hur <idoybh2@gmail.com>
2019-05-01 20:38:52 -0300

    [Squash] Settings: Add pref for qs brightness slider position [2/2] Change-Id: I583e652fad3e4f63182bab13a73188f35b8c4410 Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    -----
    
    Settings: Allow hiding auto brightness toggle [2/2]
    
    Change-Id: Iafdae6d85062435f7b2cd8426c15dde1da1c923a
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    -----
    
    Settings: Observe auto brightness mode changes
    
    We have a toggle for that in QS. Reflect if the preferences are visible
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I4d0a8198309568f963d6c9564bf08940f2869543
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    -----
    
    Settings: Add option to launch display customizations fragment
    
    Change-Id: I301ac9416994449b941b5a9e0e9aa79a76f58281
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

---
+++ src/com/android/settings/custom/fragments/DisplayCustomizations.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2021 Wave-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.custom.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.blaster.support.preference.SecureSettingMasterSwitchPreference;
+
+public class DisplayCustomizations extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "Display Customizations";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.display_customizations);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBrightnessSlider) {
+            boolean value = (boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+       return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}
