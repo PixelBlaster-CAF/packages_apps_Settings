Project-Awaken/android_packages_apps_Settings

Settings: Cleanup Network traffic
Change-Id: I9e3613b78a07089763215ab8e402e91fd0357fee
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

src/com/android/settings/awaken/fragments/NetworkTraffic.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.awaken.fragments;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.awaken.support.preferences.SystemSettingListPreference;
+import com.awaken.support.preferences.SystemSettingSeekBarPreference;
+
+public class NetworkTraffic extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private SystemSettingListPreference mLocation;
+    private SystemSettingSeekBarPreference mThreshold;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.network_traffic);
+
+        mLocation = (SystemSettingListPreference) findPreference("network_traffic_location");
+        mThreshold = (SystemSettingSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AWAKEN;
+    }
+}

PixelBlaster-CAF/packages_apps_Settings

src/com/android/settings/blaster/fragments/NetworkTraffic.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.blaster.fragments;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.blaster.support.preference.SystemSettingListPreference;
+import com.blaster.support.preference.SystemSettingSeekBarPreference;
+
+public class NetworkTraffic extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private SystemSettingListPreference mLocation;
+    private SystemSettingSeekBarPreference mThreshold;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.network_traffic);
+
+        mLocation = (SystemSettingListPreference) findPreference("network_traffic_location");
+        mThreshold = (SystemSettingSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}